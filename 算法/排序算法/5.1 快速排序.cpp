def70abfee6ec2a6b6571b273179947dc4bf30f0
xutao <xutao@zwcad.com>
2020-11-21 16:01:12 +0800

    快速排序：循环

--- 算法/排序算法/5.1 快速排序.cpp
+++ 算法/排序算法/5.1 快速排序.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include <vector>
+#include <stack>
 
 using namespace std;
 
@@ -12,8 +13,7 @@ using namespace std;
 */
 
 
-
-//快速排序
+//快速排序【递归】
 void quickSort(vector<int>& nums, int left, int right)
 {
 	if (left + 1 >= right)
@@ -45,9 +45,72 @@ void quickSort(vector<int>& nums, int left, int right)
 }
 
 
+//快速排序【循环】
+int PartSort(vector<int>& nums, int left, int right)
+{
+	if (left < right)
+	{
+		int key = nums[right];
+		while (left<right)
+		{
+			while (left<right && nums[left] <= key)
+				left++;
+			if (left < right)
+				nums[right--] = nums[left];
+
+			while (left <right && nums[right] >= key)
+				right--;
+			if (left < right)
+				nums[left++] = nums[right];
+		}
+		nums[right] = key;
+	}
+	return right;
+}
+void quickSort1(vector<int>& nums, int left, int right) 
+{
+	if (left + 1 < right)
+	{
+		stack<int> s;
+		int boundary = PartSort(nums, left, right);
+		if (boundary - 1 > left)
+		{
+			s.push(left);
+			s.push(boundary - 1);
+		}
+		if (boundary + 1 < right)
+		{
+			s.push(boundary + 1);
+			s.push(right);
+		}
+
+		while (!s.empty())
+		{
+			int end = s.top();
+			s.pop();
+			int start = s.top();
+			s.pop();
+			
+			boundary = PartSort(nums, start, end);
+			if (boundary - 1 > start)
+			{
+				s.push(start);
+				s.push(boundary - 1);
+			}
+			if (boundary + 1 < end)
+			{
+				s.push(boundary + 1);
+				s.push(end);
+			}
+		}
+	}
+
+}
+
+
 int main()
 {
 	vector<int> vec{ 5,3,1,6,4,2};
-	quickSort(vec, 0, vec.size() - 1);
+	quickSort1(vec, 0, vec.size() - 1);
 	return 0;
 }
